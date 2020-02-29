d1107cd4670fcb1f8a7a8a99e577cdfa2b0d00e3
root <root@localhost.localdomain>
2020-02-29 11:42:18 +0000

    update

---
+++ dpdk-setup-centos.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+
+if [ $1=install -o -z $1 ]; then
+	wget https://dpdk.org/rel/dpdk-17.11.2.tar.xz
+	xz -d dpdk-17.11.2.tar.xz
+	tar -xvf dpdk-17.11.2.tar
+
+	yum update
+	yum install libpcap-devel kernel*
+
+	ln -s /usr/src/kernels/$(uname -r) /lib/modules/$(uname -r)/build
+
+	sed -i 's/CONFIG_RTE_EAL_NUMA_AWARE_HUGEPAGES=y/CONFIG_RTE_EAL_NUMA_AWARE_HUGEPAGES=n/' dpdk-stable-17.11.2/config/common_linuxapp
+	sed -i 's/CONFIG_RTE_LIBRTE_VHOST_NUMA=y/CONFIG_RTE_LIBRTE_VHOST_NUMA=n/' dpdk-stable-17.11.2/config/common_linuxapp
+	sed -i 's/CONFIG_RTE_KNI_KMOD=y/CONFIG_RTE_KNI_KMOD=n/' dpdk-stable-17.11.2/config/common_linuxapp
+
+	sed -i 's/(pci_intx_mask_supported(udev->pdev))/(pci_intx_mask_supported(udev->pdev) || 1)/' dpdk-stable-17.11.2/lib/librte_eal/linuxapp/igb_uio/igb_uio.c
+
+	cd dpdk-stable-17.11.2/usertools
+	./dpdk-setup.sh
+
+elif [ $1=load ]
+
+	modprobe uio
+	ismod dpdk-stable-17.11.2/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
+        
+        ifconfig $2 down
+ 	dpdk-stable-17.11.2/usertools/dpdk-devbind.py -b igb_uio $2
+	dpdk-stable-17.11.2/usertools/dpdk-devbind.py --status
+
+elif [ $1=release ]
+
+  	dpdk-stable-17.11.2/usertools/dpdk-devbind.py -b e1000 $2
+	ifconfig $2 up
+
+else
+
+	echo "wrong flag"
+
+fi
+
+
+
