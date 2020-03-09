fe8ba5e5614c5f1a041492cc09f1db1ae5eab586
phdgang <phdgang@gmail.com>
2020-03-09 18:09:35 +0800

    update

--- go-install.sh
+++ go-install.sh
@@ -6,8 +6,8 @@ GOROOT=/usr/local/go
 tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
 echo "export GOROOT=/usr/local/go" >> /etc/profile
 echo "export PATH=$PATH:${GOROOT}/bin:$GOBIN" >> /etc/profile
-echo "export GOPROXY=https://goproxy.cn" >> /etc/profile
-echo "export GOPATH=${dir}"
+echo "export GOPROXY=https://mirrors.aliyun.com/goproxy/" >> /etc/profile
+echo "export GOPATH=${dir}" >> /etc/profile
 rm -f go1.14.linux-amd64.tar.gz
 source /etc/profile
 
