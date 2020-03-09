c925ba08bd6fe3506712bc3ce0bf443f9e63ee7f
phdgang <phdgang@gmail.com>
2020-03-09 12:07:07 +0800

    update

--- go-install.sh
+++ go-install.sh
@@ -2,6 +2,7 @@
 
 wget https://dl.google.com/go/go1.14.linux-amd64.tar.gz
 dir=`pwd`
+GOROOT=/usr/local/go
 tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
 echo "export GOROOT=/usr/local/go" >> /etc/profile
 echo "export PATH=$PATH:${GOROOT}/bin:$GOBIN" >> /etc/profile
